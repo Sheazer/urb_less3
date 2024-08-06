f068595275a84390b0a831dc14a70755a692e388
Sheazer <57393800+Sheazer@users.noreply.github.com>
2024-08-06 13:02:28 +0600

    Add files via upload

---
+++ Urbam_lessons_1/main.py
@@ -0,0 +1,5 @@
+task_count= 12
+use_time = 1.5
+course_name = 'Python'
+time_for_one_task = use_time/task_count
+print('Курс:',course_name,', всего задач:', task_count, ', затрачено часов: ', use_time,', среднее время выполнения:', time_for_one_task,'часа.')
